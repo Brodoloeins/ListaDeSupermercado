08e43a1e25c4e6380fce237e3e0463fa4c12b53e
Brodoloeins <joaofernandovascao@gmail.com>
2021-10-10 19:34:19 -0300

    toFixed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ document.querySelector('input[type=submit]').addEventListener('click',()=>{
     listaProdutos.innerHTML+=`
     <article class="produto-single">
       <h3>`+val.nome+`</h3>
-      <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
+      <h3 class="price-produto"><span>R$`+val.valor.toFixed(2)+`</span></h3>
     </article>  
     `;
   })
